f16c11dd642ce78c2f43aab6a50ba334273c233d
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-05-01 09:04:57 +0300

    first commit

---
+++ assesment3.js
@@ -0,0 +1,75 @@
+1. //Create a function called getAvailableBooks that returns an array of all available books
+function getAvailableBooks(AvailableBooks){
+    return Array .filter(AvailableBooks).map(getAvailableBooks);
+    const books = [
+        { title: 'The Blossoms of the savannah', author: 'Ole Kulet. Oxford', publicationYear: 2002,
+         isAvailable: true },
+            { title: 'Chozi la Heri', author: 'Assumpta K Matei', publicationYear: 1960,
+            isAvailable: false },
+            { title: 'Crime and defence', author: ' Skylark', publicationYear:
+            2014, isAvailable: true },
+            { title: 'The Great Nation', author: 'Sammy Liz', publicationYear: 1978,
+            isAvailable: false}
+            ];
+
+
+}
+    console.log(AvailableBooks)
+
+    
+    
+
+
+ //Create a function getBooksByAuthor that takes an author's name as an argument and
+//returns an array of all books by that author.
+function getBooksByAuthor(authorname){
+    return array .include(authorbook =>authorname).map(authorbook=>authorname)
+
+    const books = [
+        { title: 'The Blossoms of the savannah', author: 'Ole Kulet. Oxford', publicationYear: 2002,
+         isAvailable: true },
+            { title: 'Chozi la Heri', author: 'Assumpta K Matei', publicationYear: 1960,
+            isAvailable: false },
+            { title: 'Crime and defence', author: ' Skylark', publicationYear:
+            2014, isAvailable: true },
+            { title: 'The Great Nation', author: 'Sammy Liz', publicationYear: 1978,
+            isAvailable: false}
+            ];
+
+    }
+
+    
+        const (Authorbooks);
+         console.log(getbooks);
+
+
+
+
+ //Create a function addNewBook that takes a book object as an argument and adds it
+//to the library, ensuring that the new book has all required properties (title, author,
+//publicationYear, and isAvailable).
+  function AddNewBook(bookObject);{
+    const bookObject.include (title,author,publicationYear ,available)
+        const.push (bookObject);
+    }
+console.log(newbook)
+
+
+
+
+
+//Create a function checkoutBook that takes a book title as an argument and changes
+//the book's isAvailable property to false. If the book is not found in the library, the
+//function should return a message indicating that the book is not available.
+
+
+
+
+ //Create a function returnBook that takes a book title as an argument and changes the
+//book's isAvailable property to true. If the book is not found in the library, the function
+//should return a message indicating that the book does not belong to the library.//
+function returnBook(booktitle){
+    let i=0 
+    for(i=0;i<booktitle.length;i++)
+    
+}
